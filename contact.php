1e31a11a8f56996fb71db24f2421cb8af4e47215
Chetan <him@chetanreddy.com>
2018-03-23 21:23:27 -0500

    contact

--- contact.php
+++ contact.php
@@ -1,9 +1,9 @@
 <?php
 
 // configure
-$from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <demo@domain.com>'; // Add Your Email
-$subject = 'New message from contact form';
+$from = 'him@chetanreddy.com';
+$sendTo = 'him@chetanreddy.com'; // Add Your Email
+$subject = 'chetanreddy.com contact form';
 $fields = array('name' => 'Name', 'subject' => 'Subject', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
